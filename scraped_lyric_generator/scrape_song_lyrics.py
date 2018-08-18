4f0de85d7e687deff93967479cb66c74084cf4b9
Marcus Lugg <marcuslugg@googlemail.com>
2018-08-18 15:31:34 +0100

    REF: Pull scraping code out into three functions
    The code has been converted into three functions for clarity and
    reusability:
    
    * get_base_url()
    * get_soup()
    * get_relevant_links()

--- scraped_lyric_generator/scrape_song_lyrics.py
+++ scraped_lyric_generator/scrape_song_lyrics.py
@@ -8,21 +8,37 @@ URL = "https://www.azlyrics.com/m/mychemicalromance.html"
 SEARCH_STRING = 'mychemicalromance'
 
 
-parsed_url = urlparse(URL)
-base_url = parsed_url.scheme + '://' + parsed_url.netloc
+def get_base_url(url):
+    separator = '://'
+    parsed_url = urlparse(url)
+    base_url = parsed_url.scheme + separator + parsed_url.netloc
+    return base_url
 
-r = requests.get(URL, timeout=5)
-content = r.text
-soup = BeautifulSoup(content, 'html.parser')
 
-# Get all links
-link_objects = soup.find_all('a')
+def get_soup(url):
+    # Get soup (content) from URL
+    response = requests.get(url, timeout=5)
+    content = response.text
+    soup = BeautifulSoup(content, 'html.parser')
+    return soup
 
-# Sift for relevant links
-song_links = []
-for link_object in link_objects:
-    link = link_object.get('href')
-    if link is not None:
-        if SEARCH_STRING in link:
-            full_link = urljoin(base_url, link)
-            song_links.append(full_link)
+
+def get_relevant_links(soup, search_string, base_url):
+    # Get all links
+    link_objects = soup.find_all('a')
+    relevant_links = []
+
+    # Store links containing the search_string
+    for link_object in link_objects:
+        link = link_object.get('href')
+        if link is not None:
+            if search_string in link:
+                full_link = urljoin(base_url, link)
+                relevant_links.append(full_link)
+
+    return relevant_links
+
+
+base_url = get_base_url(URL)
+soup = get_soup(URL)
+song_links = get_relevant_links(soup, SEARCH_STRING, base_url)
