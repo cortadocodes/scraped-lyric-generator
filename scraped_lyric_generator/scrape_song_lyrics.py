3faaa992d8fab736f2bf46e2c9b379f4be607a12
Marcus Lugg <marcuslugg@googlemail.com>
2018-08-31 11:43:09 +0100

    REF: Put script inside function
    The function is called upon command line execution.

--- scraped_lyric_generator/scrape_song_lyrics.py
+++ scraped_lyric_generator/scrape_song_lyrics.py
@@ -11,11 +11,24 @@ URL = "https://www.azlyrics.com/m/mychemicalromance.html"
 SEARCH_STRING = 'mychemicalromance'
 
 
-# Initialise caches
-soup_cache = Cache(SOUP_CACHE_FILE)
-lyrics_cache = Cache(LYRICS_CACHE_FILE)
+def scrape_song_lyrics(soup_cache_file, lyrics_cache_file, url, search_string):
+    """
+    Scrape lyrics from url, paying attention only to content relating to search_string.
 
-# Get and save song lyrics
-song_links = get_relevant_pages(URL, SEARCH_STRING, soup_cache)
-lyrics_cache.cache = get_relevant_content(song_links, soup_cache)
-lyrics_cache.save()
+    :param str soup_cache_file: file to cache soup
+    :param str lyrics_cache_file: file to cache lyrics
+    :param str url: website to scrape
+    :param str search_string: string to search for when scraping
+    """
+    # Initialise caches
+    soup_cache = Cache(SOUP_CACHE_FILE)
+    lyrics_cache = Cache(LYRICS_CACHE_FILE)
+
+    # Get and save song lyrics
+    song_links = get_relevant_pages(URL, SEARCH_STRING, soup_cache)
+    lyrics_cache.cache = get_relevant_content(song_links, soup_cache)
+    lyrics_cache.save()
+
+
+if __name__ == '__main__':
+    scrape_song_lyrics(SOUP_CACHE_FILE, LYRICS_CACHE_FILE, URL, SEARCH_STRING)
