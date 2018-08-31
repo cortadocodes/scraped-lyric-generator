e57395f32e11fd886bdd488c7a1c2d49c91c1a4f
Marcus Lugg <marcuslugg@googlemail.com>
2018-08-31 11:34:16 +0100

    ENH: Add a second cache for scraped lyrics
    There are now two caches:
    * soup_cache: stores the scraped soup from the webpages
    * lyrics_cache: stores the lyrics from these webpages to avoid
      processing them again

--- scraped_lyric_generator/scrape_song_lyrics.py
+++ scraped_lyric_generator/scrape_song_lyrics.py
@@ -4,13 +4,18 @@ from scraped_lyric_generator.cache import Cache
 from scraped_lyric_generator.scraper import get_relevant_pages, get_relevant_content
 
 
-CACHE_FILE = os.path.join('..', 'data', 'cache' + '.pkl')
+SOUP_CACHE_FILE = os.path.join('..', 'data', 'soup_cache' + '.pkl')
+LYRICS_CACHE_FILE = os.path.join('..', 'data', 'lyrics_cache' + '.pkl')
+
 URL = "https://www.azlyrics.com/m/mychemicalromance.html"
 SEARCH_STRING = 'mychemicalromance'
 
 
-# Initialise cache
-cache = Cache(CACHE_FILE)
+# Initialise caches
+soup_cache = Cache(SOUP_CACHE_FILE)
+lyrics_cache = Cache(LYRICS_CACHE_FILE)
 
-song_links = get_relevant_pages(URL, SEARCH_STRING, cache)
-songs = get_relevant_content(song_links, cache)
+# Get and save song lyrics
+song_links = get_relevant_pages(URL, SEARCH_STRING, soup_cache)
+lyrics_cache.cache = get_relevant_content(song_links, soup_cache)
+lyrics_cache.save()
